a951c854fe9d592dbe36781d63e60fba80765e56
Antonia Andrea Carrizo Berroeta <antoocb@MacBook-Air-de-Antonia.local>
2020-03-16 19:01:00 -0300

    prueba2

--- Antoniaprueba/Antoniaprueba/Program.cs
+++ Antoniaprueba/Antoniaprueba/Program.cs
@@ -7,6 +7,7 @@ namespace Antoniaprueba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Gatos");
         }
     }
 }
